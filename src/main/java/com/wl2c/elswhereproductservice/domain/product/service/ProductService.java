9bd0a21c33156ab6118248a0fdf7d840a140e21c
kjungw1025 <kjungw1025@naver.com>
2024-07-14 18:21:38 +0900

    feat: 청약 중인 상품 목록 조회를 위한 서비스 로직 추가

--- src/main/java/com/wl2c/elswhereproductservice/domain/product/service/ProductService.java
+++ src/main/java/com/wl2c/elswhereproductservice/domain/product/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.wl2c.elswhereproductservice.domain.product.service;
 
 import com.wl2c.elswhereproductservice.domain.product.exception.ProductNotFoundException;
+import com.wl2c.elswhereproductservice.domain.product.exception.WrongProductSortTypeException;
+import com.wl2c.elswhereproductservice.domain.product.model.dto.list.SummarizedProductDto;
 import com.wl2c.elswhereproductservice.domain.product.model.dto.response.ResponseSingleProductDto;
 import com.wl2c.elswhereproductservice.domain.product.model.entity.Product;
 import com.wl2c.elswhereproductservice.domain.product.model.entity.TickerSymbol;
@@ -8,6 +10,10 @@ import com.wl2c.elswhereproductservice.domain.product.repository.ProductReposito
 import com.wl2c.elswhereproductservice.domain.product.repository.TickerSymbolRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +27,24 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final TickerSymbolRepository tickerSymbolRepository;
+
+    public Page<SummarizedProductDto> listByOnSale(String type, Pageable pageable) {
+        Sort sort = switch (type) {
+            case "latest" -> Sort.by(Sort.Order.desc("subscriptionStartDate"), Sort.Order.desc("lastModifiedAt"));
+            case "knock-in" -> Sort.by(Sort.Order.asc("knockIn").nullsLast(), Sort.Order.desc("lastModifiedAt"));
+            case "profit" -> Sort.by(Sort.Order.desc("yieldIfConditionsMet"), Sort.Order.desc("lastModifiedAt"));
+            default -> null;
+        };
+
+        if (sort == null) {
+            throw new WrongProductSortTypeException();
+        }
+
+        Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+        Page<Product> products = productRepository.listByOnSale(type, sortedPageable);
+
+        return products.map(SummarizedProductDto::new);
+    }
 
     public ResponseSingleProductDto findOne(Long id) {
         Product product = productRepository.findOne(id).orElseThrow(ProductNotFoundException::new);
