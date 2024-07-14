06c02e709e4a366395bbbd18c9693981a846b3da
kjungw1025 <kjungw1025@naver.com>
2024-07-14 18:20:18 +0900

    feat: 청약 중인 상품 목록 조회를 위한 리포지토리 메소드 추가

--- src/main/java/com/wl2c/elswhereproductservice/domain/product/repository/ProductRepository.java
+++ src/main/java/com/wl2c/elswhereproductservice/domain/product/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.wl2c.elswhereproductservice.domain.product.repository;
 
 import com.wl2c.elswhereproductservice.domain.product.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +10,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from Product p " +
+            "where p.productState = 'ACTIVE' and p.subscriptionEndDate >= CURRENT_DATE " +
+            "order by case when :sortType = 'knock-in' and p.knockIn is null then 1 else 0 end ")
+    Page<Product> listByOnSale(@Param("sortType") String sortType, Pageable pageable);
+
     @Query("select p from Product p where p.productState = 'ACTIVE' and p.id = :id ")
     Optional<Product> findOne(@Param("id") Long id);
 }
