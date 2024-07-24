c27d431213e848e590c6482f796fd8c1b25f7f12
kjungw1025 <kjungw1025@naver.com>
2024-07-24 12:10:24 +0900

    feat: ProductTickerSymbolRepository 인터페이스 추가

---
+++ src/main/java/com/wl2c/elswhereproductservice/domain/product/repository/ProductTickerSymbolRepository.java
@@ -0,0 +1,7 @@
+package com.wl2c.elswhereproductservice.domain.product.repository;
+
+import com.wl2c.elswhereproductservice.domain.product.model.entity.ProductTickerSymbol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductTickerSymbolRepository extends JpaRepository<ProductTickerSymbol, Long> {
+}
