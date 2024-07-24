20bcabf6021bbef54e60bf5bc16931a72c00f654
kjungw1025 <kjungw1025@naver.com>
2024-07-24 12:10:56 +0900

    feat: EarlyRepaymentEvaluationDatesRepository 인터페이스 추가

---
+++ src/main/java/com/wl2c/elswhereproductservice/domain/product/repository/EarlyRepaymentEvaluationDatesRepository.java
@@ -0,0 +1,7 @@
+package com.wl2c.elswhereproductservice.domain.product.repository;
+
+import com.wl2c.elswhereproductservice.domain.product.model.entity.EarlyRepaymentEvaluationDates;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EarlyRepaymentEvaluationDatesRepository extends JpaRepository<EarlyRepaymentEvaluationDates, Long> {
+}
