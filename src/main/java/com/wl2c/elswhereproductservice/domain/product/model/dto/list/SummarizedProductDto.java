e2dfc30c1b15f213e8a336068992f5fbb090aba6
kjungw1025 <kjungw1025@naver.com>
2024-07-29 16:50:33 +0900

    fix: 상품 리스트 호출을 위한 dto에 상품 유형 추가

--- src/main/java/com/wl2c/elswhereproductservice/domain/product/model/dto/list/SummarizedProductDto.java
+++ src/main/java/com/wl2c/elswhereproductservice/domain/product/model/dto/list/SummarizedProductDto.java
@@ -1,6 +1,7 @@
 package com.wl2c.elswhereproductservice.domain.product.model.dto.list;
 
 import com.querydsl.core.annotations.QueryProjection;
+import com.wl2c.elswhereproductservice.domain.product.model.ProductType;
 import com.wl2c.elswhereproductservice.domain.product.model.entity.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -18,6 +19,9 @@ public class SummarizedProductDto {
 
     @Schema(description = "상품명", example = "oo투자증권 99999")
     private final String name;
+
+    @Schema(description = "상품 유형", example = "STEP_DOWN or LIZARD or MONTHLY_PAYMENT or ETC")
+    private final ProductType productType;
 
     @Schema(description = "기초자산", example = "KOSPI200 Index / HSCEI Index / S&P500 Index")
     private final String equities;
@@ -39,6 +43,7 @@ public class SummarizedProductDto {
         this.id = product.getId();
         this.issuer = product.getIssuer();
         this.name = product.getName();
+        this.productType = product.getType();
         this.equities = product.getEquities();
         this.yieldIfConditionsMet = product.getYieldIfConditionsMet();
         this.knockIn = product.getKnockIn();
